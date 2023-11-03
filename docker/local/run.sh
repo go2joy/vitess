55db02622966a6a277e6141d4933178b13da95bb
Hung Chau <hung.chau@go2joy.vn>
2023-11-03 16:41:57 +0700

    [go2joy] feat: add custom config

--- docker/local/run.sh
+++ docker/local/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 15306:15306 -p 14200:14200 -p 14201:14201 -p 15000:15000 -p 15001:15001 -p 15991:15991 -p 15999:15999 -p 16000:16000 -d --name vitess -it vitess/local
+docker run -p 15306:15306 -p 14200:14200 -p 14201:14201 -p 15000:15000 -p 15001:15001 -p 15991:15991 -p 15999:15999 -p 16000:16000 -d --name vitess -it --log-driver json-file --log-opt max size=10m --log-opt max-file=3 vitess/local
