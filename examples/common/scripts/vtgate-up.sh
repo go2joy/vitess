8ca1870ba214eff195264f9c53120464a39b1c51
Hung Chau <hung.chau@go2joy.vn>
2023-11-06 00:08:22 +0700

    [go2joy] feat: add custom config

--- examples/common/scripts/vtgate-up.sh
+++ examples/common/scripts/vtgate-up.sh
@@ -40,6 +40,7 @@ vtgate \
   --service_map 'grpc-vtgateservice' \
   --pid_file $VTDATAROOT/tmp/vtgate.pid \
   --mysql_auth_server_impl none \
+  --grpc_max_message_size 200000000 \
   > $VTDATAROOT/tmp/vtgate.out 2>&1 &
 
 # Block waiting for vtgate to be listening
